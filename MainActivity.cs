8cfc26191bfbd8fb1b65d69246701034e6ac13f8
davidwilliamweng <38621204+davidwilliamweng@users.noreply.github.com>
2019-07-26 16:11:39 +1000

    1

---
+++ MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace calendar
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.activity_main);
+
+            var calendarView = FindViewById<CalendarView>(Resource.Id.calendarView1);
+            var txtDisplay = FindViewById<TextView>(Resource.Id.txtDisplay);
+
+            txtDisplay.Text = "Date: ";
+            calendarView.DateChange += (s,e) =>
+              {
+                  int day = e.DayOfMonth;
+                  int month = e.Month;
+                  int year = e.Year;
+                  txtDisplay.Text = "Date: " + day + "/" + month + "/" + year;
+              };
+        }
+      
+        
+
+   
+        
+	}
+}
+
